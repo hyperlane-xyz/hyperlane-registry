e64d79be58b552d8457e46e069ec60066016dda4
J M Rossy <jm.rossy@gmail.com>
2024-04-09 14:26:39 -0400

    Rename schema to schema.json and include in bundle Improve tests

--- scripts/update-chain-schema.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs'
-import { zodToJsonSchema } from "zod-to-json-schema";
-import {ChainMetadataSchemaObject} from "@hyperlane-xyz/sdk";
-
-const schema = zodToJsonSchema(ChainMetadataSchemaObject, "hyperlaneChainMetadata");
-const schemaStr = JSON.stringify(schema, null, 2);
-fs.writeFileSync(`./chains/schema`, schemaStr, 'utf8');
--- test/chains.test.ts
+++ test/chains.test.ts
@@ -1,24 +1,20 @@
 import { ChainMetadataSchema } from '@hyperlane-xyz/sdk';
 import { z } from 'zod';
-import { readAllChainYamlFiles } from './utils';
+import { chainAddresses, chainMetadata } from '../dist/index.js';
 
 describe('Chain metadata', () => {
-  it('All chains have valid metadata', () => {
-    const result = readAllChainYamlFiles('metadata.yaml');
-    for (const [chain, metadata] of Object.entries(result)) {
-      console.log('Validating metadata for', chain);
+  for (const [chain, metadata] of Object.entries(chainMetadata)) {
+    it(`${chain} metadata is valid`, () => {
       ChainMetadataSchema.parse(metadata);
-    }
-  });
+    });
+  }
 });
 
 describe('Chain addresses', () => {
   const AddressSchema = z.record(z.string());
-  it('All chain addresses are valid', () => {
-    const result = readAllChainYamlFiles('addresses.yaml');
-    for (const [chain, addresses] of Object.entries(result)) {
-      console.log('Validating addresses for', chain);
+  for (const [chain, addresses] of Object.entries(chainAddresses)) {
+    it(`${chain} addresses are valid`, () => {
       AddressSchema.parse(addresses);
-    }
-  });
+    });
+  }
 });
