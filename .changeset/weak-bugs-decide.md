979cedeab59bac2d53742aa62decc6bcce36baaa
Paul Balaji <10051819+paulbalaji@users.noreply.github.com>
2024-07-05 19:06:40 +0100

    feat: add `deployer` metadata for all chains (#89)
    resolves https://github.com/hyperlane-xyz/hyperlane-registry/issues/42
    
    - updates to latest SDK 4.1.0 which allows addition of `deployer`
    details to chain metadata
    	- update the schema with latest changes
    - add `deployer` details for all chains, both AW and PI
    - add test to check presence of `deployer` field
    
    ---------
    
    Signed-off-by: pbio <10051819+paulbalaji@users.noreply.github.com>
    Co-authored-by: J M Rossy <jm.rossy@gmail.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ .changeset/weak-bugs-decide.md
@@ -0,0 +1,5 @@
+---
+'@hyperlane-xyz/registry': minor
+---
+
+Update to @hyperlane/sdk 4.1.0 and add deployer details for each chain
--- test/unit/chains.test.ts
+++ test/unit/chains.test.ts
@@ -2,10 +2,16 @@ import { ChainMetadataSchema } from '@hyperlane-xyz/sdk';
 import { chainAddresses, chainMetadata } from '../../dist/index.js';
 import { ChainAddressesSchema } from '../../src/types.js';
 
+import { expect } from 'chai';
+
 describe('Chain metadata', () => {
   for (const [chain, metadata] of Object.entries(chainMetadata)) {
     it(`${chain} metadata is valid`, () => {
       ChainMetadataSchema.parse(metadata);
+    });
+
+    it(`${chain} metadata contains deployer details`, () => {
+      expect(metadata.deployer).not.to.be.undefined;
     });
   }
 });
