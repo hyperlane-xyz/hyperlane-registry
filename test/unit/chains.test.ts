8ccd5dd1c80229fbf77bf53ac155fd21bed51c97
pbio <10051819+paulbalaji@users.noreply.github.com>
2024-08-08 10:25:09 +0100

    move script to unit test

--- test/unit/chains.test.ts
+++ test/unit/chains.test.ts
@@ -2,7 +2,7 @@ import { ChainMetadataSchema } from '@hyperlane-xyz/sdk';
 import { chainAddresses, chainMetadata } from '../../dist/index.js';
 import { ChainAddressesSchema } from '../../src/types.js';
 
-// import { expect } from 'chai';
+import { expect } from 'chai';
 
 describe('Chain metadata', () => {
   for (const [chain, metadata] of Object.entries(chainMetadata)) {
@@ -13,6 +13,12 @@ describe('Chain metadata', () => {
     // it(`${chain} metadata contains deployer details`, () => {
     //   expect(metadata.deployer).not.to.be.undefined;
     // });
+
+    it(`${chain} metadata has gasCurrencyCoinGeckoId if deployer is Abacus Works it is a mainnet`, () => {
+      if (metadata.deployer?.name === "Abacus Works" && (metadata.isTestnet === undefined || metadata.isTestnet === false)) {
+        expect(metadata.gasCurrencyCoinGeckoId).not.to.be.undefined;
+      }
+    });
   }
 });
 
