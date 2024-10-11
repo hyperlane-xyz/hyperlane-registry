c524c21612a32ebb3807eabc8797615f88bd481c
pbio <10051819+paulbalaji@users.noreply.github.com>
2024-10-11 11:21:51 +0100

    add tests to enforce reorg period assessment framework

--- test/unit/chains.test.ts
+++ test/unit/chains.test.ts
@@ -10,12 +10,6 @@ describe('Chain metadata', () => {
       ChainMetadataSchema.parse(metadata);
     });
 
-    it(`${chain} metadata has technicalStack defined if deployer is Abacus Works, and it is a mainnet with a mailbox deployed`, () => {
-      if (metadata.deployer?.name === "Abacus Works" && !metadata.isTestnet && chainAddresses[chain]?.mailboxAddress) {
-        expect(metadata.technicalStack).not.to.be.undefined;
-      }
-    });
-
     it(`${chain} metadata contains deployer details if mailbox address is defined`, () => {
       if (chainAddresses[chain] && chainAddresses[chain].mailboxAddress) {
         expect(metadata.deployer).not.to.be.undefined;
@@ -31,6 +25,45 @@ describe('Chain metadata', () => {
     it(`${chain} metadata has gasCurrencyCoinGeckoId if deployer is Abacus Works it is a mainnet`, () => {
       if (metadata.deployer?.name === "Abacus Works" && !metadata.isTestnet) {
         expect(metadata.gasCurrencyCoinGeckoId).not.to.be.undefined;
+      }
+    });
+
+    // Values derived from reorg period assessment framework
+    // https://www.notion.so/hyperlanexyz/Reorg-period-assessment-framework-1126d35200d680cbb5f2c67b8b492d62
+    describe('Reorg period', () => {
+      // Ensure all Abacus Works mainnets have technicalStack defined
+      it(`${chain} metadata has technicalStack defined if deployer is Abacus Works, and it is a mainnet with a mailbox deployed`, () => {
+        if (metadata.deployer?.name === "Abacus Works" && !metadata.isTestnet && chainAddresses[chain]?.mailboxAddress) {
+          expect(metadata.technicalStack).not.to.be.undefined;
+        }
+      });
+
+      // Only enforce mainnet reorg period values
+      if (!metadata.isTestnet) {
+        it(`${chain} metadata has reorgPeriod of 5 if technicalStack is opstack`, () => {
+          if (metadata.technicalStack === ChainTechnicalStack.OpStack) {
+            expect(metadata.blocks?.reorgPeriod).to.equal(5);
+          }
+        });
+
+        it(`${chain} metadata has reorgPeriod of 5 if technicalStack is polygoncdk`, () => {
+          if (metadata.technicalStack === ChainTechnicalStack.PolygonCDK) {
+            expect(metadata.blocks?.reorgPeriod).to.equal(5);
+          }
+        });
+
+        it(`${chain} metadata has reorgPeriod of 5 if technicalStack is polkadotsubtrate`, () => {
+          if (metadata.technicalStack === ChainTechnicalStack.PolkadotSubstrate) {
+            expect(metadata.blocks?.reorgPeriod).to.equal(10);
+          }
+        });
+
+        it(`${chain} metadata has reorgPeriod between 0 and 5 if technicalStack is arbitrumnitro`, () => {
+          if (metadata.technicalStack === ChainTechnicalStack.ArbitrumNitro) {
+            expect(metadata.blocks?.reorgPeriod).to.be.at.least(0);
+            expect(metadata.blocks?.reorgPeriod).to.be.at.most(5);
+          }
+        });
       }
     });
   }
