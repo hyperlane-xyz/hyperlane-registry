f4809d9ca73a29b4365b4b2b3dadb38a4cfb1614
Paul Balaji <10051819+paulbalaji@users.noreply.github.com>
2024-10-28 15:54:00 +0000

    feat: initial metadata for oct-25 batch (#333)
    ### Description
    
    feat: initial metadata for oct-25 batch
    
    - add metadata for new chains
    - remove any existing deployment addresses for chains that re going to
    join the core set
    - update tests for sanity checking AW-chain metadata
    - remove deployer field from metadata-only chains that are not in
    immediate AW plans
    
    ### Backward compatibility
    
    yes
    
    ### Testing
    
    locally
    
    ---------
    
    Signed-off-by: pbio <10051819+paulbalaji@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: J M Rossy <jm.rossy@gmail.com>

---
+++ .changeset/nasty-emus-vanish.md
@@ -0,0 +1,5 @@
+---
+'@hyperlane-xyz/registry': minor
+---
+
+Add chain metadata for apechain, gravity, harmony, kaia, morph, snaxchain, zeronetwork, zksync. Update deployer details for chains not being deployed to.
--- test/unit/chains.test.ts
+++ test/unit/chains.test.ts
@@ -1,6 +1,7 @@
 import { ChainMetadataSchema, ChainTechnicalStack, EthJsonRpcBlockParameterTag } from '@hyperlane-xyz/sdk';
 import { chainAddresses, chainMetadata } from '../../dist/index.js';
 import { ChainAddressesSchema } from '../../src/types.js';
+import { isAbacusWorksChain } from '../../src/utils.js';
 
 import { expect } from 'chai';
 
@@ -44,12 +45,26 @@ describe('Chain metadata', () => {
       }
     });
 
+    // Ensure all Abacus Works mainnets have gasCurrencyCoinGeckoId defined
+    it(`${chain} metadata has gasCurrencyCoinGeckoId defined if deployer is Abacus Works`, () => {
+      if (isAbacusWorksChain(metadata) && !metadata.isTestnet) {
+        expect(metadata.gasCurrencyCoinGeckoId).not.to.be.undefined;
+      }
+    });
+
     // Values derived from reorg period assessment framework
     // https://www.notion.so/hyperlanexyz/Reorg-period-assessment-framework-1126d35200d680cbb5f2c67b8b492d62
     describe('Reorg period', () => {
+      // Ensure all Abacus Works mainnets have blocks defined
+      it(`${chain} metadata has blocks defined if deployer is Abacus Works`, () => {
+        if (isAbacusWorksChain(metadata) && !metadata.isTestnet) {
+          expect(metadata.blocks).not.to.be.undefined;
+        }
+      });
+
       // Ensure all Abacus Works mainnets have technicalStack defined
-      it(`${chain} metadata has technicalStack defined if deployer is Abacus Works, and it is a mainnet with a mailbox deployed`, () => {
-        if (metadata.deployer?.name === "Abacus Works" && !metadata.isTestnet && chainAddresses[chain]?.mailboxAddress) {
+      it(`${chain} metadata has technicalStack defined if deployer is Abacus Works`, () => {
+        if (isAbacusWorksChain(metadata) && !metadata.isTestnet) {
           expect(metadata.technicalStack).not.to.be.undefined;
         }
       });
