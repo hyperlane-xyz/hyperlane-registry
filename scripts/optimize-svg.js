14060b4799c8659f83e53bdb8d3b42b0cd89dbcd
Xaroz <jasonguo2013jg@gmail.com>
2024-12-19 12:02:15 -0600

    chore: check for raster image, update svg with raster images

--- scripts/optimize-svg.js
+++ scripts/optimize-svg.js
@@ -2,20 +2,31 @@ import fs from 'fs';
 import path from 'path';
 
 const directories = ['./chains', './deployments'];
-const MAX_FILE_SIZE = 20 * 1024; // 20KBs
+const MAX_FILE_SIZE = 100 * 1024; // 100KBs
+const RASTER_IMAGE_REGEX = /<image[^>]*>/i;
 
-function isValidSvg(filePath) {
+function isValidSvg(filePath, fileContent) {
   const fileName = path.basename(filePath);
   const stats = fs.statSync(filePath);
+  const currentFileSize = (stats.size / 1024).toFixed(2);
 
   if (!fileName.endsWith('logo.svg')) {
     console.error(`Error: File does not end with 'logo.svg' -> ${filePath}`);
-    process.exit(1); // Exit immediately if criteria is not met
+    process.exit(1);
   }
 
   if (stats.size > MAX_FILE_SIZE) {
-    console.error(`Error: File size exceeds 20KBs -> ${filePath}`);
-    process.exit(1); // Exit immediately if criteria is not met
+    console.error(
+      `Error: File size exceeds 100KBs (Current size: ${currentFileSize}KB) -> ${filePath}`,
+    );
+    process.exit(1);
+  }
+
+  if (RASTER_IMAGE_REGEX.test(fileContent)) {
+    console.error(
+      `Error: File contains an <image> tag, likely embedding a raster image -> ${filePath}`,
+    );
+    process.exit(1);
   }
 }
 
@@ -29,7 +40,8 @@ function findAndProcessSVGs(directory) {
     if (stats.isDirectory()) {
       findAndProcessSVGs(fullPath); // Recurse into subdirectories
     } else if (path.extname(fullPath) === '.svg') {
-      isValidSvg(fullPath); // Validate file, exits on failure
+      const content = fs.readFileSync(fullPath, 'utf8');
+      isValidSvg(fullPath, content); // Validate file, exits on failure
     }
   });
 }
