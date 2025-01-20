156a36bae79632c75e59e835ae38c9ebf346f9b4
Muhammed Kadir Yücel <mky515@gmail.com>
2025-01-21 00:31:47 +0300

    feat: github authenticated registry

--- src/registry/GithubRegistry.ts
+++ src/registry/GithubRegistry.ts
@@ -62,6 +62,7 @@ export class GithubRegistry extends BaseRegistry implements IRegistry {
   public readonly repoOwner: string;
   public readonly repoName: string;
   public readonly proxyUrl: string | undefined;
+  private readonly authToken: string | undefined;
 
   constructor(options: GithubRegistryOptions = {}) {
     super({ uri: options.uri ?? DEFAULT_GITHUB_REGISTRY, logger: options.logger });
@@ -72,6 +73,7 @@ export class GithubRegistry extends BaseRegistry implements IRegistry {
     this.repoOwner = pathSegments.at(-2)!;
     this.repoName = pathSegments.at(-1)!;
     this.proxyUrl = options.proxyUrl;
+    this.authToken = options.authToken;
   }
 
   getUri(itemPath?: string): string {
@@ -189,10 +191,14 @@ export class GithubRegistry extends BaseRegistry implements IRegistry {
   }
 
   public async getApiRateLimit(): Promise<GithubRateResponse['resources']['core']> {
+    const baseHeader = {
+      'X-GitHub-Api-Version': '2022-11-28',
+    };
+
     const response = await fetch(`${GITHUB_API_URL}/rate_limit`, {
-      headers: {
-        'X-GitHub-Api-Version': '2022-11-28',
-      },
+      headers: this.authToken
+        ? { ...baseHeader, Authorization: `Bearer ${this.authToken}` }
+        : baseHeader,
     });
     const { resources } = (await response.json()) as GithubRateResponse;
     return resources.core;
@@ -225,7 +231,13 @@ export class GithubRegistry extends BaseRegistry implements IRegistry {
 
   protected async fetch(url: string): Promise<Response> {
     this.logger.debug(`Fetching from github: ${url}`);
-    const response = await fetch(url);
+    const baseHeader = {
+      'X-GitHub-Api-Version': '2022-11-28',
+    };
+    const useToken = !(this.proxyUrl && url.startsWith(this.proxyUrl)) && !!this.authToken;
+    const response = await fetch(url, {
+      headers: useToken ? { ...baseHeader, Authorization: `token ${this.authToken}` } : baseHeader,
+    });
     if (!response.ok)
       throw new Error(`Failed to fetch from github: ${response.status} ${response.statusText}`);
     return response;
