10ddac303052a14f9815f225a166c6cafaaae91c
Morteza Shojaei <morteza@txfusion.io>
2025-04-14 16:27:20 +0330

    chore: move eslint rules code to monorepo

--- eslint-rules/index.js
+++
@@ -1,7 +0,0 @@
-import noRestrictedImportsFromExports from './rules/no-restricted-imports-from-exports.js';
-
-export default {
-  rules: {
-    'no-restricted-imports-from-exports': noRestrictedImportsFromExports,
-  },
-};
