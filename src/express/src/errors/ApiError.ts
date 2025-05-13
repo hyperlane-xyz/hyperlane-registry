99083519c17321c100db0608cc1cb77866faadbc
Mo Hussan <22501692+Mo-Hussain@users.noreply.github.com>
2025-05-13 12:14:07 +0100

    better handling of thrown errors

--- src/express/src/errors/ApiError.ts
+++ src/express/src/errors/ApiError.ts
@@ -1,9 +1,11 @@
 export class ApiError extends Error {
   public status: number;
+  public stack?: string;
 
-  constructor(message: string, status = 500) {
+  constructor(message: string, status = 500, stack?: string) {
     super(message);
     this.status = status;
+    this.stack = stack;
   }
 }
 
--- src/express/src/middleware/errorHandler.ts
+++ src/express/src/middleware/errorHandler.ts
@@ -1,23 +1,21 @@
 import { NextFunction, Request, Response } from 'express';
 import { ApiError } from '../errors/ApiError.js';
 
-interface ErrorResponse {
-  error: {
-    message: string;
-  };
-}
-
 export function errorHandler(err: any, _req: Request, res: Response, next: NextFunction) {
   // If we've already started sending a response, delegate to the default handler
   if (res.headersSent) {
     return next(err);
   }
 
-  const apiError = err instanceof ApiError ? err : new ApiError('Internal Server Error', 500);
-  const status = apiError.status;
-  const body: ErrorResponse['error'] = {
-    message: apiError.message,
-  };
+  const apiError =
+    err instanceof ApiError
+      ? err
+      : err instanceof Error
+      ? new ApiError(`Internal Server Error: ${err.message}`, 500, err.stack)
+      : new ApiError('Internal Server Error: unknown error', 500);
 
-  res.status(status).json(body);
+  res.status(apiError.status).json({
+    message: apiError.message,
+    stack: apiError.stack,
+  });
 }
