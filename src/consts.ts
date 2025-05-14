d7ad4446dfd8a0617ab8a5b49e52a468e323b28c
Le <6251863+ltyu@users.noreply.github.com>
2025-05-14 15:33:00 -0400

    Add WARP_ROUTE_ID_REGEX

--- src/consts.ts
+++ src/consts.ts
@@ -5,6 +5,7 @@ export const GITHUB_FETCH_CONCURRENCY_LIMIT = 5;
 
 export const CHAIN_FILE_REGEX = /chains\/([a-z0-9]+)\/([a-z]+)\.(yaml|svg)/;
 export const WARP_ROUTE_SYMBOL_DIRECTORY_REGEX = /warp_routes\/([a-zA-Z0-9]+)$/;
+export const WARP_ROUTE_ID_REGEX = /^([a-zA-Z0-9]+)\/([a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)$/;
 export const WARP_ROUTE_CONFIG_FILE_REGEX = /warp_routes\/([a-zA-Z0-9]+)\/([a-z0-9-]+)-config.yaml/;
 export const WARP_ROUTE_DEPLOY_FILE_REGEX = /warp_routes\/([a-zA-Z0-9]+)\/([a-z0-9-]+)-deploy.yaml/;
 
