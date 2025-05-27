72172651e7e66e19ba0ce453122395a767dd9a98
Le <6251863+ltyu@users.noreply.github.com>
2025-05-27 16:30:52 -0400

    Deduplicate Regex pattern

--- src/consts.ts
+++ src/consts.ts
@@ -7,9 +7,14 @@ export const CHAIN_FILE_REGEX = /chains\/([a-z0-9]+)\/([a-z]+)\.(yaml|svg)/;
 export const WARP_ROUTE_SYMBOL_DIRECTORY_REGEX = /warp_routes\/([a-zA-Z0-9]+)$/;
 
 // Must be some chars (lowercase, uppercase, numbers, . , *) with '/', followed by additional chars with optional '-' separator (e.g. ABC/xyz or ABC/xyz-mno)
-export const WARP_ROUTE_ID_REGEX = /^([a-zA-Z0-9.*]+)\/([a-z0-9]+(?:-[a-z0-9]+)*)$/;
-export const WARP_ROUTE_CONFIG_FILE_REGEX = /warp_routes\/([a-zA-Z0-9]+)\/([a-z0-9-]+)-config.yaml/;
-export const WARP_ROUTE_DEPLOY_FILE_REGEX = /warp_routes\/([a-zA-Z0-9]+)\/([a-z0-9-]+)-deploy.yaml/;
+const WARP_ROUTE_PATTERN = '([a-zA-Z0-9.*]+)/([a-z0-9-]+)';
+export const WARP_ROUTE_ID_REGEX = new RegExp(`^${WARP_ROUTE_PATTERN}$`);
+export const WARP_ROUTE_CONFIG_FILE_REGEX = new RegExp(
+  `warp_routes/${WARP_ROUTE_PATTERN}-config.yaml`,
+);
+export const WARP_ROUTE_DEPLOY_FILE_REGEX = new RegExp(
+  `warp_routes/${WARP_ROUTE_PATTERN}-deploy.yaml`,
+);
 
 export const ABACUS_WORKS_DEPLOYER_NAME = 'Abacus Works';
 export const PROXY_DEPLOYED_URL = 'https://proxy.hyperlane.xyz';
