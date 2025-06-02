c795cdd80ed53db5d42c719bb8315f7410b5d302
-f <kunalarora1729@gmail.com>
2025-06-02 14:25:00 +0100

    skip inclusivelayertestnet

--- test/unit/chains.test.ts
+++ test/unit/chains.test.ts
@@ -11,7 +11,12 @@ import { isAbacusWorksChain } from '../../src/utils.js';
 import { expect } from 'chai';
 
 describe('Chain metadata', () => {
+  const skippedChainsList = ['inclusivelayertestnet'];
   for (const [chain, metadata] of Object.entries(chainMetadata)) {
+    if (skippedChainsList.includes(chain)) {
+      continue;
+    }
+
     it(`${chain} metadata has name and domain defined`, () => {
       expect(metadata.name).not.to.be.undefined;
       expect(metadata.domainId).not.to.be.undefined;
@@ -63,7 +68,7 @@ describe('Chain metadata', () => {
       }
     });
 
-    it(`${chain} metadata has domainId within uint32 limits`, () => {
+    it(`${chain} metadata has domainId within int32 limits`, () => {
       const domainId = metadata.domainId;
       expect(domainId).to.be.at.least(0);
       // scraper db id is signed int32 - https://github.com/hyperlane-xyz/hyperlane-monorepo/issues/3121
