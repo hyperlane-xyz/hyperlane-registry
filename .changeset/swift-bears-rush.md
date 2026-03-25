768c90a8b70ba81b8766e978c0b584ad70d5701c
pbio <10051819+paulbalaji@users.noreply.github.com>
2026-03-25 19:04:32 +0000

    feat: add registry runtime subpaths

---
+++ .changeset/swift-bears-rush.md
@@ -0,0 +1,10 @@
+---
+'@hyperlane-xyz/registry': minor
+---
+
+Add slim package subpaths for apps that want registry runtime classes without importing the large root barrel:
+
+- `@hyperlane-xyz/registry/runtime`
+- `@hyperlane-xyz/registry/chainMetadata`
+- `@hyperlane-xyz/registry/chainAddresses`
+- `@hyperlane-xyz/registry/warpRouteConfigs`
---
+++ src/runtime.ts
@@ -0,0 +1,32 @@
+export {
+  CHAIN_FILE_REGEX,
+  DEFAULT_GITHUB_REGISTRY,
+  WARP_ROUTE_CONFIG_FILE_REGEX,
+} from './consts.js';
+export { BaseRegistry } from './registry/BaseRegistry.js';
+export { GithubRegistry, GithubRegistryOptions } from './registry/GithubRegistry.js';
+export { ChainFiles, IRegistry, RegistryContent, RegistryType } from './registry/IRegistry.js';
+export { MergedRegistry, MergedRegistryOptions } from './registry/MergedRegistry.js';
+export { PartialRegistry, PartialRegistryOptions } from './registry/PartialRegistry.js';
+export { HttpClientRegistry } from './registry/HttpClientRegistry.js';
+export {
+  filterWarpRoutesIds,
+  warpConfigToWarpAddresses,
+  warpRouteConfigPathToId,
+  createWarpRouteConfigId,
+} from './registry/warp-utils.js';
+export {
+  AddWarpRouteConfigOptions,
+  AddWarpRouteConfigOptionsSchema,
+  ChainAddresses,
+  ChainAddressesSchema,
+  WarpDeployConfigMap,
+  WarpRouteId,
+  WarpRouteIdSchema,
+  WarpRouteConfigMap,
+  WarpRouteFilterParams,
+  WarpRouteFilterSchema,
+  UpdateChainParams,
+  UpdateChainSchema,
+} from './types.js';
+export { isAbacusWorksChain } from './utils.js';
